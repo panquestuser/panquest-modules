36099ff77a98a15c6e3d33176f94ff3d280fea55
panquestuser <panquest@proton.me>
2023-11-22 22:42:39 +0200

    Initial Commit

--- compile.sh
+++ compile.sh
@@ -3,38 +3,6 @@
 #
 #
 
-### URLS ###
-URLS="https://altushost-swe.dl.sourceforge.net/project/qosgpl/QNAP%20NAS%20GPL%20Source/QTS%205.1.0/QTS_Kernel_5.1.0.20230808.tar.gz.0 "
-URLS+="https://altushost-swe.dl.sourceforge.net/project/qosgpl/QNAP%20NAS%20GPL%20Source/QTS%205.1.0/QTS_Kernel_5.1.0.20230808.tar.gz.1 "
-URLS+="https://master.dl.sourceforge.net/project/qosgpl/QNAP%20NAS%20Tool%20Chains/Cross%20Toolchain%20SDK%20%28x86%29%2020180115.tgz "
-
-
-if [ ! -d "QTS_Kernel_5.1.0.20230808/GPL_QTS" ] && [ ! -d "QTS_Kernel_5.1.0.20230808/x86_64-QNAP-linux-gnu" ] ; then
-
-for url in $URLS
-do
-file="$(echo $url | awk -F\/ '{print $NF}')"
-echo "Downloading $url file $file"
-[ ! -f downloads/${file} ] && curl --progress-bar --insecure -iL "$url" -o downloads/${file} || echo "File ${file} already downloaded"
-done
-
-echo "Extracting files "
-
-mv downloads/QTS_Kernel_5.1.0.20230808.tar.gz.0 downloads/QTS_Kernel_5.1.0.20230808.tar.gz
-cat downloads/QTS_Kernel_5.1.0.20230808.tar.gz.1 >> downloads/QTS_Kernel_5.1.0.20230808.tar.gz
-rm downloads/QTS_Kernel_5.1.0.20230808.tar.gz.1
-
-tar xfz downloads/QTS_Kernel_5.1.0.20230808.tar.gz -C QTS_Kernel_5.1.0.20230808 GPL_QTS/src/linux-5.10
-tar xfz "downloads/Cross%20Toolchain%20SDK%20%28x86%29%2020180115.tgz" -C QTS_Kernel_5.1.0.20230808
-
-else
-
-echo "Compilation environment OK ! Setting compilation environment parameters"
-
-fi
-
-
-
 KERNEL_SOURCE="$PWD/QTS_Kernel_5.1.0.20230808/GPL_QTS/src/linux-5.10/"
 KSRC="$KERNEL_SOURCE"
 KHOME="$PWD"
@@ -46,20 +14,44 @@ RANLIB="$KHOME/QTS_Kernel_5.1.0.20230808/toolkit/x86_64-QNAP-linux-gnu/cross-too
 LD="$KHOME/QTS_Kernel_5.1.0.20230808/toolkit/x86_64-QNAP-linux-gnu/cross-tools/bin/x86_64-QNAP-linux-gnu-ld.bfd"
 CC="$KHOME/QTS_Kernel_5.1.0.20230808/toolkit/x86_64-QNAP-linux-gnu/cross-tools/bin/x86_64-QNAP-linux-gnu-gcc"
 LD_LIBRARY_PATH="$KHOME/QTS_Kernel_5.1.0.20230808/toolkit/x86_64-QNAP-linux-gnu/fs/lib"
-KSRC="$KHOME/QTS_Kernel_5.1.0.20230808/GPL_QTS/src/linux-5.10" 
+KSRC="$KHOME/QTS_Kernel_5.1.0.20230808/GPL_QTS/src/linux-5.10"
 ARCH=x86_64
+
+### URLS ###
+KERNEL_SOURCE1="https://altushost-swe.dl.sourceforge.net/project/qosgpl/QNAP%20NAS%20GPL%20Source/QTS%205.1.0/QTS_Kernel_5.1.0.20230808.tar.gz.0 "
+KERNEL_SOURCE2="https://altushost-swe.dl.sourceforge.net/project/qosgpl/QNAP%20NAS%20GPL%20Source/QTS%205.1.0/QTS_Kernel_5.1.0.20230808.tar.gz.1 "
+TOOLKIT="https://master.dl.sourceforge.net/project/qosgpl/QNAP%20NAS%20Tool%20Chains/Cross%20Toolchain%20SDK%20%28x86%29%2020180115.tgz "
+
+if [ ! -d "$KERNEL_SOURCE" ] && [ ! -d "$CFLAGS" ]; then
+
+    [ ! -f downloads/${file} ] && wget "$url" -o downloads/KERNEL_SOURCE.tar.gz || echo "File ${file} already downloaded"
+    [ ! -f downloads/${file} ] && wget "$url" -o downloads/KERNEL_SOURCE2.tar.gz || echo "File ${file} already downloaded"
+    [ ! -f downloads/${file} ] && wget "$url" -o downloads/TOOLKIT.tar.gz || echo "File ${file} already downloaded"
+
+    echo "Extracting files "
+
+    cat downloads/KERNEL_SOURCE2.tar.gz >>downloads/KERNEL_SOURCE.tar.gz
+    tar xfz downloads/KERNEL_SOURCE.tar.gz -C QTS_Kernel_5.1.0.20230808 GPL_QTS/src/linux-5.10
+    tar xfz downloads/TOOLKIT.tar.gz -C QTS_Kernel_5.1.0.20230808
+
+else
+
+    echo "Compilation environment OK ! Setting compilation environment parameters"
+
+fi
 
 echo "Compiling..."
 cd src
 find . -exec touch {} \;
 #find $KERNEL_SOURCE -exec touch {} \;
-PARMS="`cat defines.panquest | xargs`"
+PARMS="$(cat defines.panquest | xargs)"
 echo "PARMS: $PARMS"
-make -j`nproc` KSRC=$KSRC -C $KSRC M=`pwd` "${PARMS}" modules
+make -j$(nproc) KSRC=$KSRC -C $KSRC M=$(pwd) "${PARMS}" modules
 cd ../
 
 find . -name "*.ko" -exec strip --strip-debug {} \;
-mkdir -p compiled-modules ; find src -name "*.ko" -exec cp {} compiled-modules/ \;
+mkdir -p compiled-modules
+find src -name "*.ko" -exec cp {} compiled-modules/ \;
 
 echo "Clearing compilation sources"
 find src -name "*.ko" -exec rm -f {} \;
@@ -72,5 +64,5 @@ find src -name "Module.symvers" -exec rm -f {} \;
 
 cd compiled-modules
 tar cvfz modules.tar.gz *.ko
-sha256sum="$(sha256sum modules.tar.gz |awk '{print $1}')"
-echo $sha256sum > modules.chksum
+sha256sum="$(sha256sum modules.tar.gz | awk '{print $1}')"
+echo $sha256sum >modules.chksum
